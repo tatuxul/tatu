8ae84c4f9de8b13e6e5b27390ea27fe73401a169
Woland <30810190+W-oland@users.noreply.github.com>
2023-12-13 06:40:39 +0200

    s

--- analytics.js
+++ analytics.js
@@ -1,1 +1,10 @@
-let url = navigator.language
+
+function getIP () {
+    fetch('https://ipapi.co/json/')
+            .then(response=>response.json())
+            .then((responseJson => {
+                document.write(JSON.stringify(responseJson))    
+            })) 
+    return document.write(JSON.stringify(responseJson))
+}
+
